415de0fd8b88d87c37b14191779dfc4653fdaf75
褚子铭 <chuziming@chuzimingdeMacBook-Pro.local>
2016-04-28 21:38:49 +0800

    项目创建

---
+++ HWHubClient/HWHubClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWHubClient
+//
+//  Created by ergou on 16/4/28.
+//  Copyright © 2016年 ergou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HWHubClient/HWHubClient/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  HWHubClient
+//
+//  Created by ergou on 16/4/28.
+//  Copyright © 2016年 ergou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
